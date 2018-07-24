40bae77fb1c32f721472c3a64297d5b083764d1a
seirios <646952220@qq.com>
2018-07-24 10:52:33 +0800

    index页面城市弹框完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -12,5 +12,19 @@
                 disableOnInteraction:false
             },
         });
+
+        /*弹框*/
+        $('.linkIncake').click(function () {
+            $('.city-shade').fadeIn(400);
+        })
+        $('.icon-close').click(function () {
+            $('.city-shade').fadeOut(400);
+        })
+
+        /*选择城市*/
+        $('.cities li').on('click',function () {
+            $(this).addClass('current')
+                .siblings().removeClass('current');
+        })
     })
 })(jQuery,window,document);
